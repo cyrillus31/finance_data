004059ab638bc5c9461afb9109d1165417e0ad21
Kirill Fedtsov <kirill@Mac-Air-M1.local>
2023-05-15 09:12:46 +0300

    datetime converter from CET to US/Eastern is done but untested for summer time savings

--- main.py
+++ main.py
@@ -2,12 +2,13 @@ import csv
 import datetime
 import pytz
 
+import logging
+logging.basicConfig(level=logging.DEBUG)
 
 utc = pytz.utc
-print(utc)
-# print(pytz.all_timezones)
-warsawtz = pytz.timezone("Europe/Warsaw")
-print(warsawtz)
+# logging.info(pytz.all_timezones)
+warsawtz = pytz.timezone("CET")
+nytz = pytz.timezone("US/Eastern")
 
 def convert_date_time(date: str, time: str) -> tuple[str]:
     # date format: 20230131 -> 31/01/2023
@@ -20,8 +21,11 @@ def convert_date_time(date: str, time: str) -> tuple[str]:
     hours = int(time[:2])
     minutes = int(time[2:4])
 
-    warsaw = datetime.datetime(year, month, day, hours, minutes, 0, warsawtz) 
-    print(warsaw)
+    warsaw = datetime.datetime(year, month, day, hours, minutes, 0, tzinfo=warsawtz) + datetime.timedelta(minutes=5)
+    logging.info(warsaw)
+    ny = warsaw.astimezone(nytz)
+    logging.info(ny)
+    return ny.strftime("%d/%m/%Y"), ny.strftime("%H:%M")
 
 
 with open("aapl.us.txt", "r", encoding="UTF-8") as input:
@@ -30,6 +34,8 @@ with open("aapl.us.txt", "r", encoding="UTF-8") as input:
     # reader = csv.DictReader(input, fieldnames=fieldnames)
     next(reader) #bypass the headers here
     date, time = next(reader)[2:4]
-    print(date, time)
-    convert_date_time(date, time)
+    logging.info(f"date {date} and time {time}")
 
+    # converting time from CET to US/Eastern
+    date, time = convert_date_time(date, time)
+    print(date, time)
